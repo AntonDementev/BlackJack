08a0dd95980b5849ca55312fa7214a9856531e3f
Anton Dementev <dema309@yandex.ru>
2017-03-27 03:11:31 +0700

    spep 0

--- game.rb
+++ game.rb
@@ -1,5 +1,17 @@
 #!/usr/bin/env ruby
 
+#♠♤♥♡♣♧♦♢
+#♠♥♣♦
+
+
+puts "Введите ваше имя:"
+player_name = gets.chomp
+player_money = 100
+ai_money = 100
+player_cards = []
+ai_cards = []
+
+
 loop do
 
 
