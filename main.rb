6e5dd21c6b2b1c34520519969245bd275fc2d228
Anton Dementev <dema309@yandex.ru>
2017-03-28 10:08:56 +0700

    fix main.rb

--- main.rb
+++ main.rb
@@ -4,6 +4,7 @@ require_relative 'card'
 require_relative 'card_stock'
 require_relative 'player'
 require_relative 'player_ai'
+require_relative 'player_human'
 require_relative 'black_jack'
 
 game = BlackJack.new
